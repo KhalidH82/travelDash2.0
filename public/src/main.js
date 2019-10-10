68b7933c6c2d0f23fe9dab6fd2df84d37e0257be
Khalid Hosein <khalid.hosein9@gmail.com>
2019-10-10 12:26:00 -0400

    Added hero image, changed styles on login page

--- public/src/main.js
+++ public/src/main.js
@@ -1,10 +1,32 @@
-// let pos;
-// let map;
-// function initMap() {
-//   // Set the default location and initialize all variables
-//   pos = { lat: -33.857, lng: 151.213 };
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: pos,
-//     zoom: 15
-//   });
-// }
+fetch(
+  "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
+  { mode: "no-cors" },
+  {
+    method: "POST",
+    headers: {
+      "x-rapidapi-host":
+        "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      "x-rapidapi-key": "HYaALwVmDXmsh5n3tIR5AcLh9a98p1CoC9vjsnNxT8BfGXIo7Z",
+      "content-type": "application/x-www-form-urlencoded"
+    },
+    body: {
+      inboundDate: "2019-09-10",
+      cabinClass: "business",
+      children: "0",
+      infants: "0",
+      country: "US",
+      currency: "USD",
+      locale: "en-US",
+      originPlace: "SFO-sky",
+      destinationPlace: "LHR-sky",
+      outboundDate: "2019-09-01",
+      adults: "1"
+    }
+  }
+)
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
