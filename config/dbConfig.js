1968940983781b0b97c19d34c933dc1fcdda59c7
Khalid Hosein <khalid.hosein9@gmail.com>
2019-10-11 11:13:59 -0400

    Trying to deploy to Heroku

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,26 +1,17 @@
 // Set database in Heroku
 
-// function setDatabase() {
-//   if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-//     return {
-//       database: 'travelDB',
-//       port: 5432,
-//       host: 'localhost',
-//     };
-//   } else if (process.env.NODE_ENV === 'production') {
+function setDatabase() {
+  if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
+    return {
+      database: "travelDB",
+      port: 5432,
+      host: "localhost"
+    };
+  } else if (process.env.NODE_ENV === "production") {
+    return process.env.DATABASE_URL;
+  }
+}
 
-//     return process.env.DATABASE_URL;
-//   }
-// }
+const db = setDatabase();
 
-// const db = setDatabase();
-
-// module.exports = db;
-
-const config = process.env.DATABASE_URL || {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE
-};
-
-module.exports = conf;
+module.exports = db;
